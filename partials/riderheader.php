bdb13c1b583af6c93369976546964975c7af236c
HooDSoftwares <108322650+HooDSoftware@users.noreply.github.com>
2022-09-04 11:17:28 +0300

    Update riderheader.php

--- partials/riderheader.php
+++ partials/riderheader.php
@@ -11,9 +11,9 @@
   <meta name="description" content="">
 
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/font-awesome.min.css" rel="stylesheet">
-  <link href="css/templatemo-style.css" rel="stylesheet">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/font-awesome.min.css" rel="stylesheet">
+  <link href="./css/templatemo-style.css" rel="stylesheet">
 
  
       </head>
