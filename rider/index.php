1384f83d009279b5e925657936ed965e02d85716
HooDSoftwares <108322650+HooDSoftware@users.noreply.github.com>
2022-09-04 11:50:55 +0300

    Update index.php

--- rider/index.php
+++ rider/index.php
@@ -1,6 +1,21 @@
-<?php
-include 'partials/riderheader.php';
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HooDShare</title>
+  <meta name="description" content="">
+
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+
+ 
+      </head>
+    
   <body class="about-page">
 
         <!-- Preloader -->
