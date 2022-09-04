3b17a58808cf9bf441e35d89e84e5d90008147a5
HooDSoftwares <108322650+HooDSoftware@users.noreply.github.com>
2022-09-04 10:58:25 +0300

    Add files via upload

---
+++ rider/index.php
@@ -0,0 +1,18 @@
+<?php
+include 'partials/riderheader.php';
+?>
+
+    <!--Main content-->
+    <section class="container margin-bottom-50"> 
+         
+      <div class="about-container">
+      <p>user uploads ...underconstruction!</p>
+        
+      </div>
+      
+    </section> <!-- Main content -->
+    
+    
+    <?php
+include 'partials/riderfooter.php';
+?>
